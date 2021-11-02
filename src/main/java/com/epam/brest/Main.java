0f8f5429cc0376ff823b29de6367ee8a4b2eaf0a
Ilyashuk Aleksandr <252459@gmail.com>
2021-11-02 12:11:05 +0300

    add csv_Files

--- src/main/java/com/epam/brest/Main.java
+++ src/main/java/com/epam/brest/Main.java
@@ -1,27 +1,29 @@
 package com.epam.brest;
 
-import com.epam.brest.calc.CalcImpl;
-import com.epam.brest.model.ReadData;
+import com.epam.brest.files.CSVFileReader;
+import com.epam.brest.model.ReadDataState;
 import com.epam.brest.model.Status;
 import com.epam.brest.model.StatusType;
 
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
+    Map<Integer, BigDecimal> pricePerKgMap = new CSVFileReader().readData("price_weight.csv");
+    Map<Integer, BigDecimal> pricePerKmMap = new CSVFileReader().readData("price_distance.csv");
+    try (Scanner scanner = new Scanner(System.in)) {
 
-        try (Scanner scanner = new Scanner(System.in)) {
-
-            Status currentStatus = new ReadData(scanner);
-            while (currentStatus.getType() != StatusType.EXIT) {
-                System.out.println("currentStatus " + currentStatus.getType());
-                currentStatus = currentStatus.handle();
-            }
-        }
+      Status currentStatus = new ReadDataState(scanner, pricePerKgMap, pricePerKmMap);
+      while (currentStatus.getType() != StatusType.EXIT) {
+        System.out.println("currentStatus " + currentStatus.getType());
+        currentStatus = currentStatus.handle();
+      }
     }
+  }
 
 
 }
--- src/main/java/com/epam/brest/model/ReadDataState.java
+++ src/main/java/com/epam/brest/model/ReadDataState.java
@@ -1,29 +1,34 @@
 package com.epam.brest.model;
 
 import java.math.BigDecimal;
+import java.util.Map;
 import java.util.Scanner;
 
 import static com.epam.brest.model.StatusType.*;
 
-public class ReadData implements Status {
-    Scanner scanner;
+public class ReadDataState extends AbstractStatus {
 
-    public ReadData(Scanner scanner) {
-        this.scanner = scanner;
-    }
 
-    @Override
+
+  public ReadDataState(Scanner scanner, Map<Integer, BigDecimal> pricePerKgMap, Map<Integer, BigDecimal> pricePerKmMap) {
+    this.scanner = scanner;
+    this.pricePerKgMap = pricePerKgMap;
+    this.pricePerKmMap = pricePerKmMap;
+  }
+
+
+  @Override
     public Status handle() {
         if (userData.size() < 2) {
             System.out.println(messages.get(userData.size()));
             String inputValue = scanner.next();
             if (inputValue.equalsIgnoreCase("q")) {
-                return new Exit();
+                return new ExitState();
             } else if (isCorrectValue(inputValue)) {
                 userData.add(new BigDecimal(inputValue));
             }
         } else {
-            return new Calc(scanner);
+            return new CalcState(scanner,pricePerKgMap, pricePerKmMap);
         }
 
         return this;
